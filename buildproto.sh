53e6241e2c803265a6d1e27f2459db1d51b45bdf
yoelpater <y.pater@gmail.com>
2022-07-13 17:26:17 +0800

    fix mistyped build script

--- buildproto.sh
+++ buildproto.sh
@@ -1,7 +1,6 @@
 echo "Compiling protos..."
 protoc --go_out=server/pkg/api/v1 --go_opt=paths=source_relative \
     --go-grpc_out=server/pkg/api/v1 --go-grpc_opt=paths=source_relative \
-    --dart_out=chatgo/lib/api/v1\
     --proto_path=api/proto/v1\
     --proto_path=.\
     chat.proto
